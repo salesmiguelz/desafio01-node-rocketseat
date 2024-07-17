9dbd59d9d244a646d2b5b43424287e350f281f66
Miguel Sales <miguelsales02@gmail.com>
2024-07-16 22:04:43 -0300

    fixes missing parameters logic in post route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-import { Database } from './database';
-import { buildRoutePath } from './utils/build-route-path';
+import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
 import { randomUUID } from 'node:crypto';
-import { buildServerResponse } from './utils/build-server-response';
+import { buildServerResponse } from './utils/build-server-response.js';
 
 const database = new Database;
 
@@ -10,6 +10,7 @@ export const routes = [
         method: 'POST',
         path: buildRoutePath('/tasks'),
         handler: (req, res) => {
+            const missingParams = [];
             const { title, description } = req.body;
             if(!title){
                 missingParams.push('title');
@@ -17,15 +18,16 @@ export const routes = [
             if(!description){
                 missingParams.push('description');
             }
-            if(!missingParams){
+            if(!missingParams.length){
                 const task = {
                     "id": randomUUID(),
                     "title": title,
                     "description": description,
                     "completed_at": null,
-                    "created_at": Date.now(),
-                    "updated_at": Date.now()
+                    "created_at": new Date(),
+                    "updated_at": new Date()
                 }
+
                 database.insert('/tasks', task)
                 
                 return buildServerResponse(res, '201', 'task', null, null);
